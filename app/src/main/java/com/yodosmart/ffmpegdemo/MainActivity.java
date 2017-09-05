19b36cff170f2b01fc043fd8c558d4ec2383a599
zza <zhangzhian_123@qq.com>
2017-09-05 17:29:13 +0800

    添加摄像头获取数据和YUV转Mp4

--- app/src/main/java/com/yodosmart/ffmpegdemo/MainActivity.java
+++ app/src/main/java/com/yodosmart/ffmpegdemo/MainActivity.java
@@ -5,17 +5,20 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,10 +55,23 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.rv)
     RecyclerView rv;
     MyProgressDialog dialog;
+    @BindView(R.id.tv_route_4)
+    TextView tvRoute4;
+    @BindView(R.id.bt_camera)
+    Button btCamera;
+    @BindView(R.id.bt4)
+    Button bt4;
+    @BindView(R.id.tv_route_5)
+    TextView tvRoute5;
+    @BindView(R.id.bt_yuv)
+    Button btYuv;
+    @BindView(R.id.bt5)
+    Button bt5;
 
     private List<String> dataImage = new ArrayList<>();
     private ImageAdapter adapterImage;
     private int picNum;
+    private String videoPath;
     Handler handler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message message) {
@@ -63,6 +79,9 @@ public class MainActivity extends AppCompatActivity {
                 dealResult();
             } else if (message.what == 1) {
                 Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            } else if (message.what == 2) {
+                Toast.makeText(MainActivity.this, "成功", Toast.LENGTH_SHORT).show();
                 dialog.dismiss();
             }
             return false;
@@ -90,28 +109,79 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @OnClick({R.id.bt_mp4, R.id.bt1, R.id.bt_avi, R.id.bt2, R.id.bt_h264, R.id.bt3})
+    @OnClick({R.id.bt_mp4, R.id.bt1, R.id.bt_avi, R.id.bt2, R.id.bt_h264, R.id.bt3, R.id.bt_camera, R.id.bt4, R.id.bt_yuv, R.id.bt5})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.bt_mp4:
                 openSystemFile(1);
                 break;
             case R.id.bt1:
-                ensure(tvRoute1);
+                if (!TextUtils.isEmpty(tvRoute1.getText().toString()))
+                    ensure(tvRoute1);
                 break;
             case R.id.bt_avi:
                 openSystemFile(2);
                 break;
             case R.id.bt2:
-                ensure(tvRoute2);
+                if (!TextUtils.isEmpty(tvRoute2.getText().toString()))
+                    ensure(tvRoute2);
                 break;
             case R.id.bt_h264:
                 openSystemFile(3);
                 break;
             case R.id.bt3:
-                ensure(tvRoute3);
+                if (!TextUtils.isEmpty(tvRoute3.getText().toString()))
+                    ensure(tvRoute3);
+                break;
+            case R.id.bt_camera:
+                camera(4);
+                break;
+            case R.id.bt4:
+                if (!TextUtils.isEmpty(tvRoute4.getText().toString()))
+                    ensure(tvRoute4);
+                break;
+            case R.id.bt_yuv:
+                openSystemFile(5);
+                break;
+            case R.id.bt5:
+                if (!TextUtils.isEmpty(tvRoute5.getText().toString()))
+                    compress(tvRoute5, 640, 360, 652);
                 break;
         }
+    }
+
+    private void camera(int i) {
+        videoPath = Environment.getExternalStorageDirectory().getPath() + "/Download/video.mp4";
+        Uri fileUri = Uri.fromFile(new File(videoPath));
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        //设置视频录制的最长时间
+        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 10);
+        //设置视频录制的画质
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+        startActivityForResult(intent, i);
+    }
+
+    /**
+     * 压缩按钮，开始转Mp4
+     */
+    private void compress(final TextView tvRoute, final int w_jstr, final int h_jstr, final int num_jstr) {
+        dialog.showDialog();
+        String[] videoInfo = tvRoute.getText().toString().split("/");
+        String fileName = videoInfo[videoInfo.length - 1];
+        final String[] fileNames = fileName.split("\\.");
+        new Thread(new Runnable() {
+            public void run() {
+
+                int result = yuvToMp4(tvRoute.getText().toString(), "/storage/emulated/0/Download/avtest/" + fileNames[0] + ".mp4", w_jstr, h_jstr, num_jstr);
+                //转码成功
+                if (result >= 0) {
+                    handler.sendEmptyMessage(2);
+                } else {
+                    handler.sendEmptyMessage(1);
+                }
+            }
+        }).start();
     }
 
     /**
@@ -155,27 +225,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {//是否选择，没选择就不会继续
-            Uri uri = data.getData();//得到uri，后面就是将uri转化成file的过程。
-            String[] proj = {MediaStore.Images.Media.DATA};
-            Cursor actualimagecursor = managedQuery(uri, proj, null, null, null);
-            int actual_image_column_index = actualimagecursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            actualimagecursor.moveToFirst();
-            String img_path = actualimagecursor.getString(actual_image_column_index);
-            if (img_path.endsWith(".avi") || img_path.endsWith(".mp4") || img_path.endsWith(".h264")) {
-                if (requestCode == 1) {
-                    tvRoute1.setText(img_path);
-                } else if (requestCode == 2) {
-                    tvRoute2.setText(img_path);
-                } else if (requestCode == 3) {
-                    tvRoute3.setText(img_path);
-                }
+            if (requestCode == 4) {
+                tvRoute4.setText(videoPath);
             } else {
-                Toast.makeText(this, "请选择正确的文件", Toast.LENGTH_SHORT).show();
+                Uri uri = data.getData();//得到uri，后面就是将uri转化成file的过程。
+                String[] proj = {MediaStore.Images.Media.DATA};
+                Cursor actualimagecursor = managedQuery(uri, proj, null, null, null);
+                int actual_image_column_index = actualimagecursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                actualimagecursor.moveToFirst();
+                String img_path = actualimagecursor.getString(actual_image_column_index);
+                if (img_path.endsWith(".avi") || img_path.endsWith(".mp4") || img_path.endsWith(".h264") || img_path.endsWith(".yuv")) {
+                    if (requestCode == 1) {
+                        tvRoute1.setText(img_path);
+                    } else if (requestCode == 2) {
+                        tvRoute2.setText(img_path);
+                    } else if (requestCode == 3) {
+                        tvRoute3.setText(img_path);
+                    } else if (requestCode == 5) {
+                        tvRoute5.setText(img_path);
+                    }
+                } else {
+                    Toast.makeText(this, "请选择正确的文件", Toast.LENGTH_SHORT).show();
+                }
             }
 
-        }
 
+        }
     }
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
@@ -195,5 +272,7 @@ public class MainActivity extends AppCompatActivity {
     public native int avToBitmap(String input_jstr, String output_jstr);
 
     public native int RGBToBitmap();
+
+    public native int yuvToMp4(String input_jstr, String output_jstr, int w_jstr, int h_jstr, int num_jstr);
 
 }
